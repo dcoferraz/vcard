66cd43b5a6771a6386a5b2c932f1c27f07076b14
dcoferraz <dannyphantom6@gmail.com>
2014-05-12 22:39:01 -0300

    commit inicial
    Commit inicial do projeto

---
+++ index.php
@@ -0,0 +1,254 @@
+<html dir="ltr" lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    
+    <title>Daniel Ferraz</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/reset.css"/> 
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css"/>
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,800,700,400italic|PT+Serif:400,400italic"/>
+    <!--[if IEMobile]> 
+    <link rel="stylesheet" type="text/css" href="css/iemobile.css"/>
+    <![endif]--> 
+    
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easytabs.min.js"></script>
+    <script type="text/javascript" src="js/respond.min.js"></script>
+    <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="js/jquery-ui-map.js"></script>
+    <script type="text/javascript" src="js/jquery.carouFredSel.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+</head>
+    <body>
+        <!-- Container -->
+        <section id="container">
+        
+            <!-- Header -->
+			<header> 
+            	<!-- Logo -->
+            	<div id="logo">
+                	<h2>DANIEL FERRAZ</h2>
+                    <h4>DEVELOPER</h4>
+                </div>
+                <!-- /Logo -->
+                
+                <!-- Social Icons -->
+                <ul class="socialicons">
+                	<li><a href="#" class="transition">&#x2a;</a></li>
+                </ul>
+                <!-- /Social Icons -->
+            </header>
+            <!-- /Header -->
+            
+            <!-- Content -->
+            <section id="content" >
+            
+                <!-- Profile -->
+                <div id="profile"> 
+                 	<!-- About section -->
+                	<div class="about">
+                    	<div class="photo-inner">
+                            <ul>
+                                <li><img src="images/photo.jpg" height="186" width="153" /></li>
+                                <li><img src="images/photo1.png" height="186" width="153" /></li>
+                            </ul>
+                        </div>
+                        <h1>DANIEL FERRAZ</h1>
+                        <h3>DEVELOPER</h3>
+                        <p>Creative, hard-working and everything else you are looking for!</p>
+                    </div>
+                    <!-- /About section -->
+                     
+                    <!-- Personal info section -->
+                	<ul class="personal-info">
+						<li><label>Name</label><span>Daniel Ferraz</span></li>
+                        <li><label>Birthday</label><span>July 13, 1991</span></li>
+                        <li><label>Address</label><span>Campinas, S&atilde;o Paulo</span></li>
+                        <li><label>Email</label><span class="word-wrap">ferrazdaniel5@gmail.com</span></li>
+                        <li><label>Phone</label><span>+55 019 98868-6581</span></li>
+                        <li><label>Website</label><span class="word-wrap"><a href="http://www.dcoferraz.com">www.dcoferraz.com</a></span></li>
+                    </ul>
+                    <!-- /Personal info section -->
+                </div>        
+                <!-- /Profile --> 
+
+                <!-- Menu -->
+                <nav class="menu">
+                	<ul class="tabs">
+                        <li class="tmenu"><a href="#profile" class="tab-profile"><i>&#x43;</i>Profile</a></li>
+                        <li class="tmenu"><a href="#resume"><i>&#x3a;</i>Resume</a></li>
+                        <li class="tmenu"><a href="#contact"><i>&#x21;</i>Contact</a></li>
+                    </ul>
+                    <a class="prev" id="menu-prev" href="#">&#xe073;</a>
+                    <a class="next" id="menu-next" href="#">&#xe076;</a>
+
+                </nav>
+                <!-- /Menu --> 
+                
+                <!-- Resume -->
+                <section id="resume">
+                	 <div class="timeline-section">
+                        <!-- Timeline for Employment  -->   
+                        <h3 class="main-heading">Employment</h3>   
+                        <ul class="timeline">
+                            <li>
+                                                   
+                                <div class="timelineUnit">
+                                    <h4>WEB Developer<span class="timelineDate">2014 - Present</span></h4>
+                                    <h5>PHP Junior Developer</h5>
+                                    <p>Since Feburary a Junior developer in HDN Tecnologia.</p>
+                                </div>
+                            </li>
+                            <li>
+                         
+                                <div class="timelineUnit">
+                                    <h4>Web Developer<span class="timelineDate">2013 - 2010</span></h4>
+                                    <h5>Internship</h5>
+                                    <p>Started programming with PHP on HDN Tecnologia.</p>
+                                </div>
+                            </li>
+                            <div class="clear"></div>
+                        </ul> 
+                        <!-- /Timeline for Employment  -->
+
+                        <!-- Timeline for Education  -->   
+                        <h3 class="main-heading">Education</h3>   
+                         <ul class="timeline">
+                            <li>            
+                                <div class="timelineUnit">
+                                    <h4>Unesp - Rio Claro<span class="timelineDate">2010 - present</span></h4>
+                                    <h5>Computer Science</h5>
+                                    <p></p>
+                                </div>
+                            </li>
+                            <div class="clear"></div>
+                        </ul> 
+                        <!-- /Timeline for Education  -->              
+                    </div>
+                    <div class="skills-section">
+                        <!-- Skills -->
+                        <h3 class="main-heading">Graphics and media skills</h3> 
+                        <ul class="skills">
+                            <li>
+                                <h4>Photoshop</h4>
+                                <div class="rating" data-rat="2"></div>
+
+                            </li>
+                            <li>
+                                <h4>Flash</h4>
+                                <div class="rating" data-rat="3"></div>
+                            </li>
+                            <li>
+                                <h4>Corel Draw</h4>
+                                <div class="rating" data-rat="2"></div>
+                            </li>
+                        </ul>
+                   
+                        <h3 class="main-heading">Programming Skills</h3> 
+                        <ul class="skills">
+                            <li>
+                                <h4>SQL</h4>
+                                <div class="rating" data-rat="6"></div>
+                            </li>
+                            <li>
+                                <h4>php</h4>
+                                <div class="rating" data-rat="4"></div>
+                            </li>
+                            <li>
+                                <h4>Javascript</h4>
+                                <div class="rating" data-rat="7"></div>
+                            </li>
+                            <li>
+                                <h4>SOAP</h4>
+                                <div class="rating" data-rat="5"></div>
+                            </li>
+                            <li>
+                                <h4>HTML</h4>
+                                <div class="rating" data-rat="4"></div>
+                            </li>
+                            <li>
+                                <h4>CSS</h4>
+                                <div class="rating" data-rat="4"></div>
+                            </li>
+                        </ul>
+                        
+                        <h3 class="main-heading">Language Skills</h3> 
+                        <ul class="skills">
+                            <li>
+                                <h4>English</h4>
+                                <div class="rating" data-rat="10"></div>
+
+                            </li>
+                            <li>
+                                <h4>Portuguese</h4>
+                                <div class="rating" data-rat="10"></div>
+                            </li>
+                        </ul>
+                     <!-- /Skills -->
+                     </div>
+                     <div class="clear"></div>
+                     <a href="/vcard/files/Curriculum_[Daniel_Ferraz].pdf" class="button transition" target="_blank">Download resume as PDF format</a>
+                </section>
+                <!-- /Resume --> 
+                                            
+                <!-- Contact -->
+                <section id="contact">
+                	<div id="map"></div>
+                	<!-- Contact Info -->
+                    <div class="contact-info">
+                        <h3 class="main-heading"><span>Contact info</span></h3>
+                    	<ul>
+                            <li>Campinas, S&atilde;o Paulo<br><br></li>
+                            <li>Email: ferrazdaniel5@gmail.com</li>
+                            <li>Phone: +55 019 9 8868-6581</li>
+                        </ul>
+                    </div>
+                    <!-- /Contact Info -->
+                    
+                    <!-- Contact Form -->
+                    <div class="contact-form">
+                        <h3 class="main-heading"><span>Let's keep in touch!</span></h3>
+                        <div id="contact-status"></div>
+                        <form action="" id="contactform">
+                            <p>
+                            	<input type="text" name="name" class="input" placeholder="Your Name" required>
+                            </p>
+                            <p>
+                            	<input type="text" name="email" class="input" placeholder="Your E-mail" required>
+                            </p>
+                            <p>
+                            	<label for="message">Your Message</label>
+                                <textarea name="message" cols="88" rows="6" class="textarea" placeholder="Your Message goes here!" required></textarea>
+                            </p>
+                            <input type="submit" name="submit" value="Send your message" class="button transition">
+                        </form>
+                    </div>
+                    <!-- /Contact Form -->
+                </section>
+                <!-- /contact -->  
+
+            </section>
+            <!-- /Content -->
+            
+            <!-- Footer -->
+			<footer>
+            	<div class="copyright">Copyright © 2014 Daniel Ferraz</div>
+            </footer>
+            <!-- /Footer --> 
+            
+        </section>
+		<!-- /Container -->
+
+    </body>
+</html>
